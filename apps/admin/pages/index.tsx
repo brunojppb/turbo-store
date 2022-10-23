57208660546cf0e8f888e2e58f1f503ed752d70a
Bruno Paulino <bruno@bpaulino.com>
2022-10-23 19:59:09 +0200

    Monorepo setup

---
+++ apps/admin/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../components/Navbar";
+import { Orders } from "../components/Orders";
+
+export default function Docs() {
+  return (
+    <>
+      <Navbar />
+      <Orders />
+    </>
+  );
+}
---
+++ apps/shop/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../components/Navbar";
+import { Products } from "../components/Products";
+
+export default function Web() {
+  return (
+    <>
+      <Navbar />
+      <Products />
+    </>
+  );
+}
