7852f4f7b3f8323785e5b8b630d909c7218778f9
Bruno Paulino <bruno@bpaulino.com>
2022-10-28 00:53:36 +0200

    Change button color

--- packages/ui/src/lib/Button.tsx
+++ packages/ui/src/lib/Button.tsx
@@ -6,7 +6,7 @@ const style = {
   height: "2.81rem",
   lineHeight: "2.8rem",
   borderRadius: "7px",
-  backgroundColor: "blue",
+  backgroundColor: "#444",
   color: "white",
   border: "none",
 };
